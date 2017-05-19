841806729f2e893a6a592ea735f7c75ac60530a5
Franco Bellucci <belluccifranco@gmail.com>
2017-05-19 04:29:46 -0300

    Deja pasar request tipo OPTIONS por las validaciones de JWT Agrega CORS control para permitir acceso desde cualquier origin

--- sic-api/src/main/java/sic/App.java
+++ sic-api/src/main/java/sic/App.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
@@ -14,6 +16,16 @@ public class App extends WebMvcConfigurerAdapter {
     @Bean    
     public JwtInterceptor jwtInterceptor() {
         return new JwtInterceptor();
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**");
+            }
+        };
     }
     
     @Override
